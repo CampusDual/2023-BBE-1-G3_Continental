35ea40e22b109de524cdd468d14aa3a15a0a5d8b
Artairoga <Artairoga@gmail.com>
2023-05-16 11:28:29 +0200

    ADD Hotel sin test

---
+++ src/main/java/com/hotel/Continental/controller/HotelController.java
@@ -0,0 +1,30 @@
+package com.hotel.Continental.controller;
+
+import com.hotel.Continental.model.dto.HotelDTO;
+import com.hotel.Continental.services.HotelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/hotel")
+public class HotelController {
+    @Autowired
+    private HotelService hotelService;
+
+    @GetMapping("/get")
+    public HotelDTO getHotel(@RequestBody HotelDTO hotelDTO) {
+        return hotelService.queryHotel(hotelDTO);
+    }
+
+    @GetMapping(value = "/getAll")
+    public List<HotelDTO> getAllHotels() {
+        return hotelService.queryAll();
+    }
+
+    @PostMapping(value = "/add")
+    public int addHotel(@RequestBody HotelDTO hotelDTO) {
+        return hotelService.insertHotel(hotelDTO);
+    }
+}
---
+++ src/main/java/com/hotel/Continental/model/Hotel.java
@@ -0,0 +1,43 @@
+package com.hotel.Continental.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String name;
+    @Column
+    private String address;
+
+    public int getId() {
+        return id;
+    }
+
+    public Hotel setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Hotel setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public Hotel setAddress(String address) {
+        this.address = address;
+        return this;
+    }
+}

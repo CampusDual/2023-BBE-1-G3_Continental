27a289e51140823b3a408d947a24bb430be5df07
Artairoga <Artairoga@gmail.com>
2023-05-19 10:26:30 +0200

    Solucionados Test Hoteles

--- src/test/java/com/hotel/Continental/controller/HotelControllerTest.java
+++ src/test/java/com/hotel/Continental/controller/HotelControllerTest.java
@@ -1,6 +1,7 @@
 package com.hotel.Continental.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hotel.Continental.api.IHotelService;
 import com.hotel.Continental.model.Hotel;
 import com.hotel.Continental.model.dto.HotelDTO;
 import com.hotel.Continental.service.HotelService;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -21,41 +23,50 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 class HotelControllerTest {
+    private MockMvc mockMvc;
+    @Mock
+    private IHotelService ihotelService;
+    @InjectMocks
+    HotelController hotelController;
 
-	private MockMvc mockMvc;
-	@InjectMocks
-	HotelController hotelController;
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.standaloneSetup(hotelController).build();
+    }
 
-	@BeforeEach
-	public void setup() {
-		mockMvc = MockMvcBuilders.standaloneSetup(hotelController)
-				.build();
-	}
-	/*
-	@Test
-	public void addHotelTest() throws Exception {
-		String jsonRequest = "{\"name\": \"Continental\", \"address\": \"DireccionInventada\"}";
-		mockMvc.perform(post("/hotel/add")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(jsonRequest))
-				.andExpect(status().isOk());
-	}
+    @Test
+    public void addHotelTest() throws Exception {
+        HotelDTO hotelDTO = new HotelDTO();
+        hotelDTO.setName("Continental");
+        hotelDTO.setAddress("123 Main St");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequest = objectMapper.writeValueAsString(hotelDTO);
+        MvcResult result = mockMvc.perform(post("/hotel/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isOk())
+                .andReturn();
+        String resultContent = result.getResponse().getContentAsString();
+        assertEquals("0", resultContent);
+    }
 
-	@Test
-	public void testInsertNull() throws Exception {
-		String jsonRequest = "{\"name\": null, \"address\": null}";
-		mockMvc.perform(post("/hotel/add")
-						.contentType(MediaType.APPLICATION_JSON)
-						.content(jsonRequest))
-				.andExpect(status().is(400));
-	}
-	*/
-	@Test
-	public void testInsertNothing() throws Exception {
-		HotelDTO hotelDTO = new HotelDTO();
-		hotelDTO.setName(null);
-		hotelDTO.setAddress(null);
-		mockMvc.perform(post("/hotel/add"))
-				.andExpect(status().is(400));
-	}
+    @Test
+    public void testInsertNull() throws Exception {
+        HotelDTO hotelDTO = new HotelDTO();
+        hotelDTO.setName(null);
+        hotelDTO.setAddress(null);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequest = objectMapper.writeValueAsString(hotelDTO);
+        MvcResult result = mockMvc.perform(post("/hotel/add")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequest))
+                .andExpect(status().isBadRequest())
+                .andReturn();
+
+    }
+
+    @Test
+    public void testInsertNothing() throws Exception {
+        mockMvc.perform(post("/hotel/add")).andExpect(status().isBadRequest());
+    }
 }

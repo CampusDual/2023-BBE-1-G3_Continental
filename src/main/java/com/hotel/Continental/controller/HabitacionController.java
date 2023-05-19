8ceae092b9aafbd93a04cee677fa70df60494ffb
Lordgaviota <javiercalo02@gmail.com>
2023-05-19 10:49:13 +0200

    Update test_habitaciones

--- src/main/java/com/hotel/Continental/controller/HabitacionController.java
+++ src/main/java/com/hotel/Continental/controller/HabitacionController.java
@@ -3,10 +3,12 @@ package com.hotel.Continental.controller;
 import com.hotel.Continental.model.dto.HabitacionDto;
 import com.hotel.Continental.api.IHabitacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/habitacion")
@@ -17,6 +19,11 @@ public class HabitacionController {
 
     @PostMapping(value = "/add")
     public int addHotel(@RequestBody HabitacionDto habitacionDto) {
+        if (habitacionDto.getIdHotel() == null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "IdHotel is required");
+        } else if (habitacionDto.getNumHabitacion() == 0) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "NumHabitacion is required");
+        }
         return habitacionService.insertHabitacion(habitacionDto);
     }
 }
--- src/test/java/com/hotel/Continental/controller/HabitacionControllerTest.java
+++ src/test/java/com/hotel/Continental/controller/HabitacionControllerTest.java
@@ -1,11 +1,14 @@
 package com.hotel.Continental.controller;
 
+import com.hotel.Continental.api.IHabitacionService;
+import com.hotel.Continental.api.IHotelService;
 import com.hotel.Continental.model.Hotel;
 import com.hotel.Continental.model.dto.HabitacionDto;
 import com.hotel.Continental.model.dto.HotelDTO;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -18,6 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class HabitacionControllerTest {
 
     private MockMvc mockMvc;
+    @Mock
+    private IHabitacionService ihabitacionService;
     @InjectMocks
     HabitacionController habitacionController;
 
@@ -25,6 +30,27 @@ public class HabitacionControllerTest {
     public void setup() {
         mockMvc = MockMvcBuilders.standaloneSetup(habitacionController)
                 .build();
+    }
+    @Test
+    public void testInsertHabitación() throws Exception {
+        HabitacionDto habitacionDto = new HabitacionDto();
+        habitacionDto.setIdHotel(new Hotel(19));
+        habitacionDto.setNumHabitacion(103);
+        mockMvc.perform(post("/habitacion/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"idHotel\":19,\"numHabitacion\":103}"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testInsertNull() throws Exception {
+        HabitacionDto habitacionDto = new HabitacionDto();
+        habitacionDto.setIdHotel(null);
+        habitacionDto.setNumHabitacion(null);
+        mockMvc.perform(post("/habitacion/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"idHotel\":null,\"numHabitacion\":null}"))
+                .andExpect(status().is(400));
     }
 
     @Test

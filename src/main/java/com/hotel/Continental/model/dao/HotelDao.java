c020b234349d6195f047caf78de420030067bd6c
Tomas Paz  Pena <tomaspazpena@gmail.com>
2023-05-25 10:53:22 +0200

    Arquetipo ontimize creado

--- src/main/java/com/hotel/Continental/model/dao/HotelDao.java
+++
@@ -1,5 +0,0 @@
-package com.hotel.Continental.model.dao;
-
-import com.hotel.Continental.model.Hotel;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface HotelDao extends JpaRepository<Hotel,Integer> {}

68fe834197bfdef932ade5af5942df3139a840b7
Tomas Paz  Pena <tomaspazpena@gmail.com>
2023-05-29 14:59:48 +0200

    Avance query habitacionesLibres

--- Continental-api/src/main/java/com/hotel/Continental/api/core/service/IRoomService.java
+++ Continental-api/src/main/java/com/hotel/Continental/api/core/service/IRoomService.java
@@ -1,6 +1,7 @@
 package com.hotel.Continental.api.core.service;
 
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 
 import java.util.List;
 import java.util.Map;
@@ -9,4 +10,5 @@ public interface IRoomService {
     public EntityResult roomQuery(Map<?, ?> keyMap, List<?> attrList);
     public EntityResult roomInsert(Map<?, ?> attrMap);
     public EntityResult roomDelete(Map<?, ?> keyMap);
+    public EntityResult freeRoomsQuery(Map<String, Object> keyMap, List<String> attrList) throws OntimizeJEERuntimeException;
 }
--- Continental-model/src/main/java/com/hotel/Continental/model/core/service/RoomService.java
+++ Continental-model/src/main/java/com/hotel/Continental/model/core/service/RoomService.java
@@ -3,18 +3,18 @@ package com.hotel.Continental.model.core.service;
 import com.hotel.Continental.api.core.service.IHotelService;
 import com.hotel.Continental.api.core.service.IRoomService;
 import com.hotel.Continental.model.core.dao.HotelDao;
+import com.ontimize.jee.common.db.SQLStatementBuilder.BasicExpression;
 import com.hotel.Continental.model.core.dao.RoomDao;
+import com.ontimize.jee.common.db.SQLStatementBuilder;
 import com.ontimize.jee.common.dto.EntityResult;
+import com.ontimize.jee.common.exceptions.OntimizeJEERuntimeException;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Lazy
 @Service("RoomService")
@@ -39,4 +39,19 @@ public class RoomService implements IRoomService {
         attrMap.put("roomdowndate", new Timestamp(Calendar.getInstance().getTimeInMillis()));
         return this.daoHelper.update(this.roomDao, attrMap, keyMap);
     }
+
+    @Override
+    public EntityResult freeRoomsQuery(Map<String, Object> keyMap, List<String> attrList)
+            throws OntimizeJEERuntimeException {
+        List<String> columnsRooms = new ArrayList<>();
+        columnsRooms.add(RoomDao.IDHABITACION);
+        EntityResult roomsId = this.daoHelper.query(this.roomDao, keyMap, columnsRooms, RoomDao.QUERY_FREE_ROOMS);
+        List<Integer> ids = (List<Integer>) roomsId.get(RoomDao.IDHABITACION);
+        Map<String, Object> idRoomsToExclude = new HashMap<>();
+        BasicExpression bexp1 = new BasicExpression(new SQLStatementBuilder.BasicField(RoomDao.IDHABITACION), SQLStatementBuilder.BasicOperator.NOT_IN_OP, idRoomsToExclude);
+        idRoomsToExclude.put(SQLStatementBuilder.ExtendedSQLConditionValuesProcessor.EXPRESSION_KEY, bexp1);
+        EntityResult rooms = this.roomQuery(idRoomsToExclude, attrList);
+        return roomsId;
+    }
+
 }

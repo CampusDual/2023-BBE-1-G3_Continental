c944637dd6408fafcc56917b63fe0a369c40ed73
Carla53 <carlagd192@gmail.com>
2023-05-31 11:54:22 +0200

    hotel_changes

--- Continental-model/src/main/java/com/hotel/Continental/model/core/dao/HotelDao.java
+++ Continental-model/src/main/java/com/hotel/Continental/model/core/dao/HotelDao.java
@@ -14,4 +14,5 @@ public class HotelDao extends OntimizeJdbcDaoSupport {
     public static final String ID = "id";
     public static final String ADDRESS = "direccion";
     public static final String NAME = "nombre";
+    public static final String HOTELDOWNDATE = "hoteldowndate";
 }
--- Continental-model/src/main/java/com/hotel/Continental/model/core/service/HotelService.java
+++ Continental-model/src/main/java/com/hotel/Continental/model/core/service/HotelService.java
@@ -2,6 +2,7 @@ package com.hotel.Continental.model.core.service;
 
 import com.hotel.Continental.api.core.service.IHotelService;
 import com.hotel.Continental.api.core.service.IUserService;
+import com.hotel.Continental.model.core.dao.BookDao;
 import com.hotel.Continental.model.core.dao.HotelDao;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
@@ -10,10 +11,10 @@ import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.sql.Timestamp;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @Lazy
 @Service("HotelService")
@@ -33,9 +34,14 @@ public class HotelService implements IHotelService {
         return this.daoHelper.insert(hotelDao, attrMap);
     }
 
-    public EntityResult hotelDelete(Map<?, ?> keyMap) {
+    @Override
+    public EntityResult hotelUpdate(Map<String, Object> attrMap, Map<?, ?> keyMap) {
+        return this.daoHelper.update(this.hotelDao, attrMap, keyMap);
+    }
+
+    public EntityResult hotelDelete(Map<?, ?> keyMap) throws ParseException {
         Map<Object, Object> attrMap = new HashMap<>();
-        attrMap.put("hotel_down_date", new Timestamp(Calendar.getInstance().getTimeInMillis()));
+        attrMap.put(HotelDao.HOTELDOWNDATE, new Timestamp(System.currentTimeMillis()));
         return this.daoHelper.update(this.hotelDao, attrMap, keyMap);
     }
 }

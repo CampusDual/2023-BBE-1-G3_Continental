0b157066f656c692a164a297efe01c741c590848
Lordgaviota <javiercalo02@gmail.com>
2023-06-05 14:20:56 +0200

    TestQueryHotel

--- Continental-model/src/test/java/com/Hotel/Continental/model/core/service/HotelServiceTest.java
+++ Continental-model/src/test/java/com/Hotel/Continental/model/core/service/HotelServiceTest.java
@@ -15,12 +15,14 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import javax.management.Query;
+import javax.swing.text.Keymap;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -56,21 +58,67 @@ public class HotelServiceTest {
 
     @Nested
     @TestInstance(Lifecycle.PER_CLASS)
-    public class hotelServiceGet {
+    public class hotelServiceQuery {
         @Test
-        void testInsertHotel() {
+        void testQueryHotel() {
+            EntityResult er = new EntityResultMapImpl();
+            er.setCode(0);
+            er.put("nombre", List.of("prueba"));
+            er.put("direccion", List.of("direccionPrueba"));
+
+            when(daoHelper.query(any(HotelDao.class), anyMap(), anyList())).thenReturn(er);
+
+            Map<String, Object> keyMap = new HashMap<>();
+            keyMap.put("id", 0);
+
+            List<String> attrList = List.of("id", "nombre", "direccion");
+
+            EntityResult queryResult = hotelService.hotelQuery(keyMap, attrList);
+
+            assertEquals(0, queryResult.getCode(), "El código de retorno de hotelInsert() no es igual a 0");
+
+        }
+
+        @Test
+        void testQueryHotelEmpty() {
+            EntityResult er = new EntityResultMapImpl();
+
+            when(daoHelper.query(any(HotelDao.class), anyMap(), anyList())).thenReturn(er);
+
+            EntityResult queryResult = hotelService.hotelQuery(new HashMap<>(), List.of());
+
+            assert queryResult.isEmpty();
+        }
+    }
+
+    @Nested
+    @TestInstance(Lifecycle.PER_CLASS)
+    public class testUpdateHotel {
+        @Test
+        void testUpdateHotel() {
             EntityResult er = new EntityResultMapImpl();
             er.setCode(0);
             Map<String, Object> hotelToInsert = new HashMap<>();
 
+            hotelToInsert.put("id", 1);
             hotelToInsert.put("nombre", "prueba");
             hotelToInsert.put("direccion", "direccionPrueba");
 
             when(daoHelper.insert(any(HotelDao.class), anyMap())).thenReturn(er);
+            Map<String, Object> hotelToUpdate = new HashMap<>();
 
-            EntityResult result = hotelService.hotelInsert(hotelToInsert);
+            hotelToUpdate.put("nombre", "actualizado");
+            hotelToUpdate.put("direccion", "direccionPruebaactualizada");
+
+            Map<String, Object> list = new HashMap<>() {{
+                put("id", 1);
+            }};
+            when(daoHelper.update(any(HotelDao.class), anyMap(), anyMap())).thenReturn(er);
+
+            EntityResult result = hotelService.hotelUpdate(list, hotelToUpdate);
 
             assertEquals(0, result.getCode());
+
         }
     }
 }

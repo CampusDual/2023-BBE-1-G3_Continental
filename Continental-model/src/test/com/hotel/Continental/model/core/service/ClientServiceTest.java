e67fe1c97a429807b983eff8c51b841734b599cd
Artairoga <Artairoga@gmail.com>
2023-06-05 12:35:24 +0200

    Test WIP

---
+++ Continental-model/src/test/com/hotel/Continental/model/core/service/ClientServiceTest.java
@@ -0,0 +1,7 @@
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+@ExtendWith(MockitoExtension.class)
+public  class ClientServiceTest {
+}

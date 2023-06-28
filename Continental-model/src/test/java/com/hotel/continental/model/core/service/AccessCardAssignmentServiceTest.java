f4ba3e1a1b5f58c470cd1c781e4d1c298d8a678c
Artairoga <Artairoga@gmail.com>
2023-06-28 21:00:14 +0200

    fix:Nombre

--- Continental-model/src/test/java/com/hotel/continental/model/core/service/AccessCardAssignmentServiceTest.java
+++ Continental-model/src/test/java/com/hotel/continental/model/core/service/AccessCardAssignmentServiceTest.java
@@ -23,7 +23,7 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MockitoExtension.class)
-public class AccessCardAssignmentTest {
+public class AccessCardAssignmentServiceTest {
     @Mock
     static DefaultOntimizeDaoHelper daoHelper;
     @InjectMocks

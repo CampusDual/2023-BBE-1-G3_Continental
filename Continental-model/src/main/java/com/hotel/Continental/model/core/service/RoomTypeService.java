169dc1a09ed2ad8c03343584089c8937e520c18f
Artairoga <Artairoga@gmail.com>
2023-07-04 13:31:45 +0200

    wip

--- Continental-model/src/main/java/com/hotel/Continental/model/core/service/RoomTypeService.java
+++ Continental-model/src/main/java/com/hotel/Continental/model/core/service/RoomTypeService.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Lazy
@@ -21,18 +23,24 @@ public class RoomTypeService implements IRoomTypeService {
     IOntimizeDaoHelper daoHelper;
     @Override
     public EntityResult roomtypeUpdate(Map<?, ?> attrMap, Map<?, ?> keyMap) {
+        //Compruebo que me envian la clave
         if (keyMap.get(RoomTypeDao.TYPEID) == null) {
             EntityResult er = new EntityResultMapImpl();
             er.setCode(1);
             er.setMessage(ErrorMessages.NECESSARY_KEY);
             return er;
         }
+        //Compruebo que me envian los atributos a modificar
         if (attrMap.isEmpty()) {
             EntityResult er = new EntityResultMapImpl();
             er.setCode(1);
             er.setMessage(ErrorMessages.NECESSARY_DATA);
             return er;
         }
+        //Compruebo que el tipo de habitacion existe
+        Map<String, Object> attrMapRoomType = new HashMap<>();
+        attrMapRoomType.put(RoomTypeDao.TYPEID, keyMap.get(RoomTypeDao.TYPEID));
+        EntityResult erRoomType = this.daoHelper.query(this.roomtypeDao, attrMapRoomType, List.of());
         return this.daoHelper.update(this.roomtypeDao, attrMap, keyMap);
     }
 }

e86fabf184d90d824471cdafb597eb3848edde8e
Tomas Paz  Pena <tomaspazpena@gmail.com>
2023-07-04 13:53:16 +0200

    insertrefrigerators done

--- Continental-model/src/main/java/com/hotel/Continental/model/core/service/RefrigeratorsService.java
+++ Continental-model/src/main/java/com/hotel/Continental/model/core/service/RefrigeratorsService.java
@@ -2,17 +2,29 @@ package com.hotel.continental.model.core.service;
 
 import com.hotel.continental.api.core.service.IRefrigeratorsService;
 import com.hotel.continental.model.core.dao.RefrigeratorsDao;
+import com.hotel.continental.model.core.dao.RoomDao;
 import com.hotel.continental.model.core.tools.ErrorMessages;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
+import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import com.ontimize.jee.server.dao.IOntimizeDaoHelper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Lazy
 @Service("RefrigeratorsService")
 public class RefrigeratorsService implements IRefrigeratorsService {
+    @Autowired
+    RefrigeratorsDao refrigeratorsDao;
+    @Autowired
+    RoomDao roomDao;
+    @Autowired
+    DefaultOntimizeDaoHelper daoHelper;
     @Override
     public EntityResult refrigeratorsInsert(Map<?, ?> attrMap) {
         //Compruebo que me envia una capacidad y un room_id
@@ -38,7 +50,17 @@ public class RefrigeratorsService implements IRefrigeratorsService {
             erError.setMessage(ErrorMessages.CAPACITY_NOT_POSITIVE);
             return erError;
         }
-        
-        return null;
+        Map<String, Object> filter = new HashMap<>();
+        filter.put(RoomDao.IDHABITACION, attrMap.get(RefrigeratorsDao.ROOM_ID));
+        EntityResult rooms = this.daoHelper.query(this.roomDao, filter, List.of(RoomDao.IDHABITACION));
+
+        if (rooms.calculateRecordNumber()==0) {
+            EntityResult er = new EntityResultMapImpl();
+            er.setCode(EntityResult.OPERATION_WRONG);
+            er.setMessage(ErrorMessages.ROOM_NOT_EXIST);
+            return er;
+        }
+
+        return this.daoHelper.insert(this.refrigeratorsDao, attrMap);
     }
 }

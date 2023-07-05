2b1c431d2b009c272f6a7327061e7d31fdac143b
Carla53 <carlagd192@gmail.com>
2023-07-05 13:51:51 +0200

    fridgeStockDaos

---
+++ Continental-ws/src/main/java/com/hotel/continental/ws/core/rest/RefrigeratorStockRestController.java
@@ -0,0 +1,19 @@
+package com.hotel.continental.ws.core.rest;
+
+import com.hotel.continental.api.core.service.IRefrigeratorStockService;
+import com.ontimize.jee.server.rest.ORestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/refrigeratorStock")
+public class RefrigeratorStockRestController extends ORestController<IRefrigeratorStockService> {
+    @Autowired
+    private IRefrigeratorStockService refrigeratorStockService;
+
+    @Override
+    public IRefrigeratorStockService getService() {
+        return this.refrigeratorStockService;
+    }
+}

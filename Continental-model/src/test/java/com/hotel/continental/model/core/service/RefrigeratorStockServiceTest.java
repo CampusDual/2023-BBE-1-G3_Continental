e8a313da9d6efcd1c4bff72bcf4f1feeeb51788e
Carla53 <carlagd192@gmail.com>
2023-07-12 13:04:15 +0200

    test wp

--- Continental-model/src/test/java/com/hotel/continental/model/core/service/RefrigeratorStockServiceTest.java
+++ Continental-model/src/test/java/com/hotel/continental/model/core/service/RefrigeratorStockServiceTest.java
@@ -6,6 +6,7 @@ import com.hotel.continental.model.core.tools.ErrorMessages;
 import com.ontimize.jee.common.dto.EntityResult;
 import com.ontimize.jee.common.dto.EntityResultMapImpl;
 import com.ontimize.jee.server.dao.DefaultOntimizeDaoHelper;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -15,6 +16,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
@@ -23,7 +25,6 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class RefrigeratorStockServiceTest {
@@ -41,7 +42,7 @@ class RefrigeratorStockServiceTest {
 
     @ParameterizedTest(name = "Test case {index} : {0}")
     @MethodSource("refrigeratorDefaultUpdate")
-    void testRegrigeratorInsert(String testCaseName, Map<String, Object> attrMap, Map<?, ?> keyMap, EntityResult expectedResult, List<Supplier> mock) {
+    void testRefrigeratorDefaultUpdate(String testCaseName, Map<String, Object> attrMap, Map<?, ?> keyMap, EntityResult expectedResult, List<Supplier> mock) {
         //For each test case, execute the mock,to make sure the mock is called
         mock.forEach(Supplier::get);
         EntityResult result = refrigeratorStockSrv.refrigeratorDefaultUpdate(attrMap, keyMap);
@@ -59,13 +60,13 @@ class RefrigeratorStockServiceTest {
                         Map.of(RefrigeratorStockDao.PRODUCTID, 2),
                         createEntityResult(EntityResult.OPERATION_SUCCESSFUL, ""),
                         List.of(
-                                (Supplier) () -> {
+                                () -> {
                                     EntityResult erQueryProductDefault = new EntityResultMapImpl();
                                     erQueryProductDefault.put(ProductsDao.PRODUCTID, List.of(2));
 
                                     return Mockito.when(daoHelper.query(any(ProductsDao.class), anyMap(), anyList())).thenReturn(erQueryProductDefault);
                                 },
-                                (Supplier) () -> {
+                                () -> {
                                     EntityResult erQueryFridgeDefault = new EntityResultMapImpl();
                                     erQueryFridgeDefault.put(RefrigeratorStockDao.STOCKID, List.of(2));
 
@@ -90,13 +91,13 @@ class RefrigeratorStockServiceTest {
                         Map.of(RefrigeratorStockDao.PRODUCTID, 2),
                         createEntityResult(EntityResult.OPERATION_SUCCESSFUL, ""),
                         List.of(
-                                (Supplier) () -> {
+                                () -> {
                                     EntityResult erQueryProductDefault = new EntityResultMapImpl();
                                     erQueryProductDefault.put(ProductsDao.PRODUCTID, List.of(2));
 
                                     return Mockito.when(daoHelper.query(any(ProductsDao.class), anyMap(), anyList())).thenReturn(erQueryProductDefault);
                                 },
-                                (Supplier) () -> {
+                                () -> {
                                     EntityResult erQueryFridgeDefault = new EntityResultMapImpl();
 
                                     return Mockito.when(daoHelper.query(any(RefrigeratorStockDao.class), anyMap(), anyList())).thenReturn(erQueryFridgeDefault);
@@ -148,6 +149,72 @@ class RefrigeratorStockServiceTest {
                         Map.of(RefrigeratorStockDao.PRODUCTID, 2),
                         createEntityResult(EntityResult.OPERATION_WRONG, ErrorMessages.STOCK_NOT_POSITIVE),
                         List.of()
+                )
+                //endregion
+        );
+    }
+
+    @Disabled
+    @ParameterizedTest(name = "Test case {index} : {0}")
+    @MethodSource("refrigeratorStockUpdate")
+    void testRefrigeratorStockUpdate(String testCaseName, Map<String, Object> keyMap, Map<String, Object> attrMap, EntityResult expectedResult, List<Supplier> mock) {
+        //For each test case, execute the mock,to make sure the mock is called
+        mock.forEach(Supplier::get);
+        EntityResult result = refrigeratorStockSrv.refrigeratorStockUpdate(attrMap, keyMap);
+        // Assert
+        assertEquals(expectedResult.getMessage(), result.getMessage());
+        assertEquals(expectedResult.getCode(), result.getCode());
+    }
+
+    private static Stream<Arguments> refrigeratorStockUpdate() {
+        return Stream.of(
+                //region Test Case 1: update RefrigeratorDefault stock with correct data
+                Arguments.of(
+                        "update RefrigeratorDefault stock with correct data",
+                        Map.of(RefrigeratorStockDao.REFRIGERATORID, 2, RefrigeratorStockDao.PRODUCTID, 2),
+                        Map.of(RefrigeratorStockDao.STOCK, 2),
+                        createEntityResult(EntityResult.OPERATION_SUCCESSFUL, ""),
+                        List.of(
+                                () -> {
+                                    EntityResult erQueryProductDefault = new EntityResultMapImpl();
+                                    erQueryProductDefault.put(ProductsDao.PRODUCTID, List.of(2));
+                                    erQueryProductDefault.put(ProductsDao.NAME, List.of("name"));
+                                    erQueryProductDefault.put(ProductsDao.PRICE, List.of(12));
+
+                                    return Mockito.when(daoHelper.query(any(ProductsDao.class), anyMap(), anyList())).thenReturn(erQueryProductDefault);
+                                },
+                                () -> {
+                                    EntityResult erQueryFridge = new EntityResultMapImpl();
+                                    erQueryFridge.put(RefrigeratorStockDao.STOCKID, List.of(1, 2));
+                                    erQueryFridge.put(RefrigeratorStockDao.REFRIGERATORID, List.of(-1, 1));
+                                    erQueryFridge.put(RefrigeratorStockDao.STOCK, List.of(5, 1));
+
+                                    return Mockito.when(daoHelper.query(any(RefrigeratorStockDao.class), anyMap(), anyList())).thenReturn(erQueryFridge);
+                                },
+                                () -> {
+                                    EntityResult erQueryFridgeDefault = new EntityResultMapImpl();
+                                    erQueryFridgeDefault.put(RefrigeratorStockDao.REFRIGERATORID, List.of(2));
+                                    erQueryFridgeDefault.put(RefrigeratorStockDao.STOCKID, List.of(2));
+                                    erQueryFridgeDefault.put(RefrigeratorStockDao.PRODUCTID, List.of(2));
+
+                                    EntityResult erQueryFridge = new EntityResultMapImpl();
+                                    erQueryFridge.put(RefrigeratorStockDao.REFRIGERATORID, List.of(-1));
+                                    erQueryFridge.put(RefrigeratorStockDao.STOCKID, List.of(5));
+                                    erQueryFridge.put(RefrigeratorStockDao.PRODUCTID, List.of(2));
+
+                                    return Mockito.when(daoHelper.query(any(RefrigeratorsDao.class), anyMap(), anyList()))
+                                            .thenReturn(erQueryFridge, erQueryFridgeDefault);
+                                },
+                                (Supplier) () -> {
+                                    EntityResult erUpdateFridgeDefault = new EntityResultMapImpl();
+                                    erUpdateFridgeDefault.put(RefrigeratorStockDao.STOCKID, List.of(2));
+                                    erUpdateFridgeDefault.put(RefrigeratorStockDao.STOCK, List.of(5));
+                                    erUpdateFridgeDefault.put(RefrigeratorStockDao.PRODUCTID, List.of(2));
+                                    erUpdateFridgeDefault.put(RefrigeratorStockDao.REFRIGERATORID, List.of(-1));
+
+                                    return Mockito.when(daoHelper.update(any(RefrigeratorStockDao.class), anyMap(), anyMap())).thenReturn(erUpdateFridgeDefault);
+                                }
+                        )
                 )
                 //endregion
         );
